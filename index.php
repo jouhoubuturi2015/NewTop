86a823b27e17540ed88299680d7bb5031049ca96
1210960058 <1210960058u@hiro.kindai.ac.jp>
2015-06-11 15:09:17 +0900

    ひな型の作成

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html><!--HTML5で書く-->
+<html lang="ja"><!--日本語で書く-->
+	<head>
+		<meta  charset="UTF-8">  <!--HTML5では文字コードにUTF-8を強く推奨している--><!--空要素だがHTML５なので終わりを「/>」とはしない-->
+		<style type="text/css" ></style>
+		<title>INDEX</title>
+	</head>
+	<body>
+<p>test</p>
+	</body>
+
+
+
+
+
+
+
+
+
+
+</html>
