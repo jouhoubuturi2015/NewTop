8ed95a468a18921ae6978e706a0a3dc30791b7b4
ta98330 <hare.night@gmail.com>
2015-06-11 15:13:58 +0900

    タイトル

--- index.php
+++ index.php
@@ -2,8 +2,8 @@
 <html lang="ja"><!--日本語で書く-->
 	<head>
 		<meta  charset="UTF-8">
-		<style type="text/css" >2015年度　情報物理研究室</style>
-		<title>INDEX</title>
+		<style type="text/css" ></style>
+		<title>2015年度　情報物理研究室</title>
 	</head>
 	<body>
 <p>test</p>
