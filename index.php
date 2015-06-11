d11cc7807def3d87f951326f11f790b4de928508
ta98330 <hare.night@gmail.com>
2015-06-11 18:19:52 +0900

    ファビコン

--- index.php
+++ index.php
@@ -4,6 +4,8 @@
         <meta  charset="UTF-8">
         <title>2015年度情報物理研究室</title>
         <link rel="stylesheet" href="base.css">
+        <link rel="shortcut icon" href="images/profile/favi.gif">
+
     </head>
     <body>
         <header>
