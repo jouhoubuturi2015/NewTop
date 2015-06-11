faefc6782d4449adc2fb5fe6ec85a7b76ef68161
ta98330 <hare.night@gmail.com>
2015-06-11 15:30:00 +0900

    indexファイル　インデント調整

--- index.php
+++ index.php
@@ -1,26 +1,29 @@
 <!DOCTYPE html>
 <html lang="ja">
-	<head>
-		<meta  charset="UTF-8">
-		<link rel="stylesheet" href="base.css">
-		<title>2015年度　情報物理研究室</title>
-	</head>
-	<body>
+    <head>
+        <meta  charset="UTF-8">
+        <title>2015年度情報物理研究室</title>
+        <link rel="stylesheet" href="base.css">
+    </head>
+    <body>
         <header>
-            <h1>2015年度　情報物理研究室</h1>
+            <h1>2015年度情報物理研究室</h1>
         </header>
-        <p>test</p>
-        <p>second ==test</p>
-	
-    
-    
-    
-    
-    
-    
-    
-    
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
         
     </body>
-
 </html>
