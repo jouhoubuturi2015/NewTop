99548c42b36020d63dc5737a34d092261d2afac4
ta98330 <hare.night@gmail.com>
2015-06-16 14:20:26 +0900

    ファビコンのリンクを訂正・大見出しにリンク追加

--- index.php
+++ index.php
@@ -4,12 +4,12 @@
         <meta  charset="UTF-8">
         <title>2015年度情報物理研究室</title>
         <link rel="stylesheet" href="base.css">
-        <link rel="shortcut icon" href="images/profile/favi.gif">
+        <link rel="shortcut icon" href="images/favi.gif">
 
     </head>
     <body>
         <header>
-            <h1>2015年度情報物理研究室</h1>
+            <h1><a href="index.php">2015年度情報物理研究室</a></h1>
         </header>
         
         <section id="page_body">
