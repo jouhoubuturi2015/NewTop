cc24f69a20bad21f9da7e273645d014b4445981b
ta98330 <hare.night@gmail.com>
2015-06-19 01:22:51 +0900

    マウスオーバーテスト

--- move.js
+++ move.js
@@ -9,3 +9,31 @@ $(function(){
             return false;
         });
     });
+
+/*----------------------------------マウスオーバー基本形（jquery使用）-----------------*/
+$('#hoge').hover(
+    function(){ //マウスオーバー処理 
+        
+    },
+    function(){ //マウスアウト処理
+        
+    }
+);
+
+
+/*----------------------------------マウスオーバー基本形（jquery使用）-----------------*/
+$(function(){
+    $('.profile').hover(function() {
+        $(this).animate({
+            'width':'200px',
+            'height':'200px',
+            'marginLeft':'3px'
+        }, 300);//左を3px空ける事でテキストを右にずらす
+    },function() {
+        $(this).animate({
+            'width':'100px',
+            'height':'100px',
+            'marginLeft':'0px'
+        }, 300);
+    });
+});
