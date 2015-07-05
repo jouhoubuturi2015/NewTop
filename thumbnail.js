4934656e1d82c8c581ea9caf8c7c643bd5a5619f
1210960058 <1210960058u@hiro.kindai.ac.jp>
2015-07-05 15:18:55 +0900

    リンク先のサムネイルが表示されるように変更 アンカーにsamuと言うクラス名をつける

---
+++ thumbnail.js
@@ -0,0 +1,26 @@
+/* 自動サムネイル表示プラグイン for jQuery
+2009.1 By KaZuhiro FuRuhata  */
+//http://jump-up.info/jquery/thumbnail.html
+//↑サイト参考
+
+$.fn.thumbnail = function(baseURL){
+    var serverURL = "http://capture.heartrails.com/320x240/round/border/shadow?";
+    return this.each(function(){
+        $(this).hover(
+            function(e){
+                var url = $(this).attr("href");
+                $("#jqThumbnail").attr("src",serverURL+url);
+                $("#jqThumbnail").css("left",e.pageX+10);
+                $("#jqThumbnail").css("top",e.pageY+10);
+                $("#jqThumbnail").show();
+            },
+            function(){
+                $("#jqThumbnail").hide();
+            }
+        );
+    });
+}
+$(function(){
+    $("body").append("<img src='' id='jqThumbnail' width='320' height='240' style='position:absolute;display:none'>");
+    $("a.samu").thumbnail();//a.○○で○○をクラス名とする。○○を消さば全てaがなる
+});
