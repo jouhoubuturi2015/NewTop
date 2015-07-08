e6001194430eeaf45969005bd8a369cb9c5c1de7
ta98330 <hare.night@gmail.com>
2015-07-08 12:02:34 +0900

    プロフィール画面

--- move.js
+++ move.js
@@ -13,6 +13,20 @@ $(function(){
 
 /*----------------------------------マウスオーバープロフィール画面（jquery使用）-----------------*/
 $(function(){
+    $('#matsubara').hover(
+        function(){ //マウスオーバー処理
+            $('#default').css("display","none");
+            $('#matsubara_profile').css("display","inline");
+        },
+        function(){ //マウスアウト処理
+            $('#matsubara_profile').css("display","none");
+            $('#default').css("display","inline");
+        }
+    );
+});
+
+
+$(function(){
     $('#misawa').hover(
         function(){ //マウスオーバー処理
             $('#default').css("display","none");
@@ -24,6 +38,80 @@ $(function(){
         }
     );
 });
+
+
+$(function(){
+    $('#nishi').hover(
+        function(){ //マウスオーバー処理
+            $('#default').css("display","none");
+            $('#nishi_profile').css("display","inline");
+        },
+        function(){ //マウスアウト処理
+            $('#nishi_profile').css("display","none");
+            $('#default').css("display","inline");
+        }
+    );
+});
+
+
+$(function(){
+    $('#nizato').hover(
+        function(){ //マウスオーバー処理
+            $('#default').css("display","none");
+            $('#nizato_profile').css("display","inline");
+        },
+        function(){ //マウスアウト処理
+            $('#nizato_profile').css("display","none");
+            $('#default').css("display","inline");
+        }
+    );
+});
+
+
+$(function(){
+    $('#okada').hover(
+        function(){ //マウスオーバー処理
+            $('#default').css("display","none");
+            $('#okada_profile').css("display","inline");
+        },
+        function(){ //マウスアウト処理
+            $('#okada_profile').css("display","none");
+            $('#default').css("display","inline");
+        }
+    );
+});
+
+
+$(function(){
+    $('#sone').hover(
+        function(){ //マウスオーバー処理
+            $('#default').css("display","none");
+            $('#sone_profile').css("display","inline");
+        },
+        function(){ //マウスアウト処理
+            $('#sone_profile').css("display","none");
+            $('#default').css("display","inline");
+        }
+    );
+});
+
+
+$(function(){
+    $('#toyota').hover(
+        function(){ //マウスオーバー処理
+            $('#default').css("display","none");
+            $('#toyota_profile').css("display","inline");
+        },
+        function(){ //マウスアウト処理
+            $('#toyota_profile').css("display","none");
+            $('#default').css("display","inline");
+        }
+    );
+});
+
+
+
+
 
 
 
@@ -46,6 +134,30 @@ $(function(){
     }
 });
 */
+
+/*
+var memberlist = ["matsubara", "misawa", "nishi", "nizato", "okada", "sone", "toyota"];
+    for(var i=0 ; i<memberlist.length; i++){
+
+        var name = memberlist[i];
+        $(function memberpro(name));
+    }
+
+$(function memberpro(name){
+    $('#'+name).hover(
+        function mover(name){ //マウスオーバー処理
+            $('#default').css("display","none");
+            $('#'+name+'_profile').css("display","inline");
+        },
+        function mout(name){ //マウスアウト処理
+            $('#'+name+'_profile').css("display","none");
+            $('#default').css("display","inline");
+        }
+    );
+});
+*/
+
+
 
 
 
